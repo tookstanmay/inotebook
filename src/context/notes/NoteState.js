5960941c6fdf4bbd737286be4d775438b8cfd18d
Tanmay Sharma <tookstanmay@gmail.com>
2024-07-02 12:53:12 +0530

    build, vercel.json created

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import noteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const host = "http://localhost:5000";
+  const host = process.env.BACKEND_URL;
 
   const notesInitial = [];
 
